5a64577bb58177fe0073553c619a43cf2f9f39a4
Bryce Courneya <bcourneya1@my.bcit.ca>
2018-11-24 22:52:49 -0800

    media queries
    Added media queries for savings calculator and the nav bar, and made a few small edits on both.

---
+++ savCalcPref.js
@@ -0,0 +1,5 @@
+var make = document.getElementById("make");
+var year = document.getElementById("year");
+var model = document.getElementById("model");
+var make = document.getElementById("make");
+var make = document.getElementById("make");
