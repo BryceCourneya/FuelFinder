70390218210228662fed442b2f37ed9cdfad8cd2
Bryce Courneya <bcourneya1@my.bcit.ca>
2018-11-29 14:11:57 -0800

    Changed a lot of stuff!
    Got database working, changed style, media queries etc..

--- savCalcPref.js
+++ savCalcPref.js
@@ -1,5 +1,3 @@
-var make = document.getElementById("make");
-var year = document.getElementById("year");
-var model = document.getElementById("model");
-var make = document.getElementById("make");
-var make = document.getElementById("make");
+
+
+
