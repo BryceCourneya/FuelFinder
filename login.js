8e4ca3188bcc7a16762f7844dc62dce3c5821bf9
Luigi Alcuaz <luigi.alcuaz0197@gmail.com>
2018-12-02 17:28:02 -0800

    changes to preferences

--- login.js
+++ login.js
@@ -47,10 +47,11 @@ firebase.auth().onAuthStateChanged(function(user) {
     // User is signed in.
     var fb = database.ref();
     var user = firebase.auth().currentUser;
+
     user.getIdToken().then(function(accessToken) {
       document.getElementById('notSignedIn').style.display="none";
       document.getElementById('signedIn').style.display="block";
-      document.getElementById('accountName').innerHTML =  user.email;
+      document.getElementById('accountName').innerHTML = user.email;
     });
 
     //Car make
@@ -121,26 +122,81 @@ function editP(){
 
 //account preferences, information is put into database
 function edit() {
+
   var make = document.getElementById("make").value;
   var model = document.getElementById("model").value;
   var year = document.getElementById("year").value; 
-  //var station = document.getElementById("year").value; 
+  var station = document.getElementById("station").value; 
   var jerryCans = document.getElementById("jerryCans").value; 
-  var hours = document.getElementById("timeH").value;
-  var minutes = document.getElementById("timeM").value;  
-  var maxDistance = document.getElementById("maxDistance").value; 
+  var jerryCanSize = document.getElementById("jerryCanSize").value; 
 
-  var user = firebase.auth().currentUser;
-  database.ref('users/'+user.uid).update( {
-    "make": make,
-    "model": model,
-    "year": year,
-    //"station" : station,
-    "jerry cans" : jerryCans,
-    "hours" : hours,
-    "minutes" : minutes,
-    "maxDistance" : maxDistance
-  });
+  if (document.getElementById('closest').checked) {
+    var station = "Closest US";
+  }
 
-  window.location.href="myAccount.html"; 
+  if (document.getElementById('lowest').checked) {
+    var station = document.getElementById("lowestKM").value;
+  }
+
+  if (document.getElementById('check1').checked) {
+    var jerryCans = document.getElementById("jerryCans").value;
+    var jerryCanSize = document.getElementById("jerryCanSize").value;
+  } else {
+    var jerryCans = 0;
+    var jerryCanSize = 0;
+  }
+
+  if (document.getElementById('check4').checked) {
+    
+    if (document.getElementById('regular').checked) {
+      var fuelGrade = "Regular";
+    }
+    if (document.getElementById('midgrade').checked) {
+      var fuelGrade = "Midgrade";
+    }
+    if (document.getElementById('premium').checked) {
+      var fuelGrade = "Premium";
+    }
+    if (document.getElementById('diesel').checked) {
+      var fuelGrade = "Diesel";
+    }
+  } else {
+      var fuelGrade = "Regular";
+  }
+
+  if (make.trim() == "" || model.trim() == "" || year.trim() == "") {
+    var notComplete = true;
+  } else {
+    var notComplete = false;
+  }
+
+  /* Car types */
+  if (((make.toLowerCase() == "honda" && model.toLowerCase() == "civic")
+  || (make.toLowerCase() == "toyota" && model.toLowerCase() == "corolla"))) {
+    var carSupport = true;
+  } else {
+    var carSupport = false;
+  }
+
+  if (notComplete == false && carSupport == true) {
+
+    var user = firebase.auth().currentUser;    
+    database.ref('users/'+user.uid).update( {
+      "model" : model,
+      "make" : make,
+      "year" : year,
+      "station" : station,
+      "jerryCans" : jerryCans,
+      "jerryCanSize" : jerryCanSize,
+      "fuelGrade" : fuelGrade
+    });
+    
+    window.location.href="myAccount.html";
+
+  } else if (notComplete == true) {
+    window.alert("You must enter the make, model, and year of the car!");
+  } else if (carSupport == false) {
+    window.alert("Sorry! The Savings Calculator does not support your vehicle at this time. \nPlease enter another vehicle");
+  }
+
 }
