0326eb768f6864ed99997eac51285dc0633aede5
Kris Chan <bigmidget489@gmail.com>
2018-12-03 01:06:48 -0800

    k sort of got most of it working, still fixing tho almost done
    ye read the main log thanks

--- gasStation.js
+++ gasStation.js
@@ -1,32 +1,103 @@
 var slideIndex = 1;
-        showSlides(slideIndex);
+showSlides(slideIndex);
 
-        // Next/previous controls
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide-animation");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
-        // Thumbnail image controls
-        function currentSlides(n) {
-            showSlides(slideIndex = n);
-        }
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("slide-animation");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {
-                slideIndex = 1
-            }
-            if (n < 1) {
-                slideIndex = slides.length
-            }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-        }
+// Thumbnail image controls
+function currentSlides(n) {
+    showSlides(slideIndex = n);
+}
+
+function formsubmit() {
+    var rating = document.getElementById("ratingnumber").value;
+
+    var ratingref = firebase.database().ref().child("Station1");
+
+    ratingref.child('rating/').on('value', function (snap) {
+        document.getElementById("ratingnumber").value = (snap.val());
+    });
+
+
+
+    var comment = document.getElementById("comment").value;
+
+    /* var bigreview = document.createElement("div");
+    bigreview.setAttribute("id", "bigreview");*/
+
+
+    var reviewdiv = document.createElement("div");
+    reviewdiv.setAttribute("class", "newreviewholder");
+    reviewdiv.setAttribute("id", "newreviewholder");
+
+
+    var reviewer = document.createElement("div");
+    reviewer.setAttribute("class", "reviewer");
+    reviewer.setAttribute("id", "reviewer");
+    reviewer.innerHTML = "Test Man";
+
+
+    var reviewnumber = document.getElementById("ratingnumber").value;
+
+    var numberholder = document.createElement("div");
+
+    var reviewnumber2 = document.createElement("div");
+    reviewnumber2.setAttribute("id", "reviewnumber2");
+    reviewnumber2.setAttribute("class", "reviewnumber2");
+    reviewnumber2.innerHTML = reviewnumber;
+
+    var reviewcomment = document.createElement("div");
+    reviewcomment.setAttribute("id", "reviewcomment");
+    reviewcomment.setAttribute("class", "reviewcomment");
+    reviewcomment.innerHTML = comment;
+
+    var namennumber = document.createElement("div");
+    namennumber.setAttribute("id", "namennumber");
+
+    var reviewcount = 0;
+    
+    if (reviewnumber >= 8.0){
+        reviewnumber2.style.backgroundColor = "blue";
+    } else if (reviewnumber >= 6.0){
+        reviewnumber2.style.backgroundColor = "greenyellow";
+    } else if (reviewnumber >= 5.0) {
+        reviewnumber2.style.backgroundColor = "yellow";
+    } else if(reviewnumber >= 3.5){
+        reviewnumber2.style.backgroundColor = "orange"
+    } else {
+        reviewnumber2.style.backgroundColor = "red";
+    }
+    
+        
+    if (reviewcount == 0){
+    document.getElementById("bigreview").appendChild(reviewdiv);
+    document.getElementById("newreviewholder").appendChild(namennumber);
+    document.getElementById("namennumber").appendChild(reviewer);
+    document.getElementById("namennumber").appendChild(reviewnumber2);
+
+    document.getElementById("newreviewholder").appendChild(reviewcomment);
+    } 
+    
+}
--- gasStationList.js
+++ gasStationList.js
@@ -1,34 +1,11 @@
 $(document).ready(function () {
-    /*$("#search").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".stationname *").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });*/
 
-    var $search = $("#search").on('input', function () {
-        /*$btns.removeClass('active');*/
+    var $search = $("#search2").on('input',function(){
+        //$btns.removeClass('active');
         var matcher = new RegExp($(this).val(), 'gi');
-        $('.stationbox').show().not(function () {
-            return matcher.test($(this).find('.stationname' ).text())
+        $('.stationbox').show().not(function(){
+            return matcher.test($(this).find('.stationname').text())
         }).hide();
     })
     
-    var allstations = $(".stationbox");
-    
-    var station = $("h1.stationname").text();
-    
-    var location = 
-    $("div.stationlocation").text();
-   
-    var distance =  $("div.distancecontainer").text();
-    
-    var price = $("span.pricecontainer").text();
-    
-    var dbref = firebase.database().ref().child("Stations");
-    
-    dbref.on('value', snap => console.log(snap.val()));
-    
-    
 });
-
